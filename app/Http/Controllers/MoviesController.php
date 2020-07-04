2ca61c39d35c4ea91131c3ace65be6278b6909a2
mahmoud alghool <mahmoud@alghool.net>
2020-07-04 10:49:12 +0200

    movies index and show

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\servers\Tmdb;
-use Illuminate\Support\Facades\Http;
 
 class MoviesController extends Controller
 {
@@ -11,31 +9,34 @@ class MoviesController extends Controller
     public function index()
     {
         $tmdb = resolve('tmdb');
-        $popularMovies = $tmdb->popularMovies()->getResult();
-        dump($popularMovies);
 
-        $popularMovies2 = $tmdb->popularMovies()->getResponse();
-        dd($popularMovies2);
+        $popularMovies = $tmdb->popularMovies()->getResult()->slice(0,10);
+        $topRatedMovies = $tmdb->moviesTopRated()->getResult()->slice(0,10);
+        $nowPlayingMovies = $tmdb->moviesNowPlaying()->getResult()->slice(0,10);
+        $genresArray = $tmdb->moviesGenre()->get();
 
-        $genresArray = Http::withToken(config('services.tmdb.token'))
-            ->get(config('services.tmdb.link'). 'genre/movie/list')
-            ->json();
-
-        $genres = collect($genresArray['genres'])->mapWithKeys(function($genre){
+        $genres = collect($genresArray)->mapWithKeys(function($genre){
             return [$genre['id'] => $genre['name']];
         });
 
-        dd($genres);
+        $data = [
+            'popularMovies' => $popularMovies,
+            'nowPlayingMovies' => $nowPlayingMovies,
+            'topRatedMovies' => $topRatedMovies,
+            'genres' => $genres
+        ];
 
-        return view('movies.index',[
-            'popularMovies' => $popularMovies
-        ]);
-        //
+         return view('index',$data);
+
     }
 
     public function show($id)
     {
-        //
-    }
+        $tmdb = resolve('tmdb');
+
+        $movie = $tmdb->with(['credits','videos','images'])->movie($id)->get();
+
+        return view('show',['movie' => $movie]);
+     }
 
 }
--- app/servers/Tmdb.php
+++ app/servers/Tmdb.php
@@ -5,9 +5,16 @@ use Illuminate\Support\Facades\Http;
 class Tmdb
 {
     private $http;
-    private $popularMovies = 'movie/popular';
     private $response = null;
     private $resultKey = null;
+    private $returnType = 'collection';
+    private $append = [];
+
+    private $popularMovies = 'movie/popular';
+    private $moviesGenre = 'genre/movie/list';
+    private $moviesNowPlaying = 'movie/now_playing';
+    private $moviesTopRated = 'movie/top_rated';
+    private $movie = 'movie/';
 
     public function __construct( )
     {
@@ -15,11 +22,37 @@ class Tmdb
     }
 
     public function getResponse(){
-        return $this->response;
+        $this->clear();
+        return $this->cast($this->response);
     }
 
     public function getResult(){
-        return $this->response[$this->resultKey] ?? $this->response;
+        $this->clear();
+        return $this->cast($this->response[$this->resultKey] ?? $this->response);
+    }
+
+    public function get(){
+        return $this->getResult();
+    }
+
+    public function with($append){
+        if(is_array($append)){
+            $this->append = array_merge($this->append, $append);
+        }
+        else{
+            $this->append[] = $append;
+        }
+        return $this;
+    }
+
+    public function moviesGenre(){
+        $this->callServer($this->moviesGenre, 'genres');
+        return $this;
+    }
+
+    public function movie($movieID){
+        $this->callServer($this->movie.$movieID, '');
+        return $this;
     }
 
     public function popularMovies(){
@@ -27,9 +60,38 @@ class Tmdb
         return $this;
     }
 
+    public function moviesNowPlaying(){
+        $this->callServer($this->moviesNowPlaying, 'results');
+        return $this;
+    }
+
+    public function moviesTopRated(){
+        $this->callServer($this->moviesTopRated, 'results');
+        return $this;
+    }
+
     private function callServer($endPoint, $resultKey){
-        $this->response = $this->http->get(Config('services.tmdb.link').$endPoint)->json();
+        $this->response = $this->http->get(Config('services.tmdb.link').$endPoint.$this->getAppendString())->json();
         $this->resultKey = $resultKey;
     }
 
+    private function cast($resultArray){
+        return $this->returnType == 'collection' ? collect($resultArray) : $resultArray;
+    }
+
+    private function clear(){
+        $this->append = [];
+    }
+
+    private function getAppendString(){
+        $return = '';
+        if(!empty($this->append)){
+            $return = '?append_to_response=';
+            foreach ($this->append as $append){
+                $return .= $append .',';
+            }
+            $return = substr($return, 0, -1);
+        }
+        return $return;
+    }
 }
